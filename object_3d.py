28269edcf68e503d80ec9371a5d9adcb46a8d36d
Guillem <felisdedios@gmail.com>
2020-11-29 01:00:34 +0100

    Updated class

--- object_3d.py
+++ object_3d.py
@@ -3,6 +3,9 @@ from stl import mesh
 import numpy as np
 import stl
 
+# rotate along Y
+# main_body.rotate([0.0, 0.5, 0.0], math.radians(90))
+
 
 class Object:
 
@@ -11,39 +14,70 @@ class Object:
         self.y = 0
         self.z = 0
         self.mesh = mesh
+        self.width = self.mesh.x.max() - self.mesh.x.min()
+        self.length = self.mesh.y.max() - self.mesh.y.min()
+        self.height = self.mesh.z.max() - self.mesh.z.min()
 
-        def get_outer_cube(obj):
-            width = obj.x.max() - obj.x.min()
-            length = obj.y.max() - obj.y.min()
-            height = obj.z.max() - obj.z.min()
-            return width, length, height
-
-        self.box = get_outer_cube(mesh)
+    def pos(self):
+        return self.x, self.y, self.z
 
     def distance(self, obj):
         return math.sqrt((obj.x - self.x) ** 2 + (obj.y - self.y) ** 2 + (obj.z - self.z) ** 2)
 
-    def combine(self, obj, output_f='result.stl'):
-        combined = mesh.Mesh(numpy.concatenate([self.mesh.data, obj.data]))
+    def distance_x(self, obj):
+        return obj.x - self.x
+
+    def distance_y(self, obj):
+        return obj.y - self.y
+
+    def distance_z(self, obj):
+        return obj.z - self.z
+
+    def combine_n_save(self, obj, output_f='result.stl'):
+        combined = mesh.Mesh(np.concatenate([self.mesh.data, obj.mesh.data]))
 
         combined.save(output_f, mode=stl.Mode.BINARY)
 
-    def save(self, output_f='result.stl'):
-        self.mesh.save(output_f, mode=stl.Mode.BINARY)
+    def copy(self):
+        return Object(mesh.Mesh(self.mesh.data.copy()))
 
-    def translate(self, step: np.float32, padding: np.float32, axis):
+    def translate(self, distance, axis):
         if 'x' == axis:
             items = 0, 3, 6
-            self.x += step + padding
+            self.x += distance
         elif 'y' == axis:
             items = 1, 4, 7
-            self.y += step + padding
+            self.y += distance
         elif 'z' == axis:
             items = 2, 5, 8
-            self.z += step + padding
+            self.z += distance
         else:
             raise RuntimeError('Unknown axis %r, expected x, y or z' % axis)
 
         # _solid.points.shape == [:, ((x, y, z), (x, y, z), (x, y, z))]
-        self.mesh.points[:, items] += step + padding
+        self.mesh.points[:, items] += distance
+
+    def intersects_with(self, obj):
+        return (self.x <= obj.x and self.x + self.width >= obj.x + obj.width) and \
+               (self.y <= obj.y and self.y + self.length >= obj.y + obj.length) and \
+               (self.z <= obj.z and self.z + self.height >= obj.z + obj.height)
+
+    def move_to(self, position: (float, float, float)):
+        for point, axis in zip(position, ('x', 'y', 'z')):
+            if 'x' == axis:
+                self.translate(point - self.x, axis)
+            if 'y' == axis:
+                self.translate(point - self.y, axis)
+            if 'z' == axis:
+                self.translate(point - self.z, axis)
+
+    def move(self, variations: (float, float, float)):
+        for variation, axis in zip(variations, ('x', 'y', 'z')):
+            if 'x' == axis:
+                self.translate(variation, axis)
+            if 'y' == axis:
+                self.translate(variation, axis)
+            if 'z' == axis:
+                self.translate(variation, axis)
+
 
---
+++ tests.py
@@ -0,0 +1,11 @@
+import numpy as np
+from stl import mesh
+import object_3d
+import stl
+objects_path = 'tests/stl_binary/'
+
+
+
+
+
+
